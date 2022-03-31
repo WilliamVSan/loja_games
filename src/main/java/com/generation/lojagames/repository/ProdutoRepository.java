3f6262e6d4c45ffafe6cdbea8979a2074cd2ee7d
William <86013044+WilliamVSan@users.noreply.github.com>
2022-03-31 16:38:35 -0300

    Update ProdutoRepository.java

--- src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.lojagames.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
